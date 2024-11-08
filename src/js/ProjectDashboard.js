f59258b1489e6595432a02e3c291f2593bd1a5fe
Apostolos Karamanis <apostkaram@gmail.com>
2024-11-08 16:36:44 +0200

    minor changes

--- src/js/ProjectDashboard.js
+++ src/js/ProjectDashboard.js
@@ -1,20 +1,88 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "../css/Project.css";
 import "../css/App.css";
+
+import { supabase } from "./SupabaseClient";
+import TimeStampFormatter
+ from './TimeStampFormatter';
+
 
 
 import { useParams } from 'react-router-dom';
 
 const ProjectDashboard = () => {
 
-    const  {uuid} = useParams();
+    const {uuid} = useParams();
+    const [projects, setProjects] = useState([]);
+    const [error, setError] = useState(null);
+    const [actions, setActions] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        fetchProjects();
+        if(projects){
+            fetchActions();
+        }
+    }, [uuid]);
+
+    async function fetchProjects() {
+        try{
+            console.log("Fetching projects ... ");
+
+        let { data: projects, error } = await supabase
+            .from("projects")
+            .select("*")
+            .eq("uuid",uuid)
+            .single();
+        if (error) {
+            throw error;
+        }
+
+        setProjects(projects);
+
+        console.log("Projects fetched successfully", projects);
+        } catch (error) {
+        console.error("Error fetching projects:", error.message);
+        setError(error.message);
+        } finally {
+        setLoading(false);
+        }
+    }
+
+    async function fetchActions(){
+        try{
+            console.log("Fetching actions ... ");
+
+        let { data: actions, error } = await supabase
+            .from("actions")
+            .select("*")
+            .eq("project_id", projects.id);
+
+        if (error) {
+            throw error;
+        }
+
+        setActions(actions);
+
+        console.log("Actions fetched successfully", actions);
+        } catch (error) {
+        console.error("Error fetching actions:", error.message);
+        setError(error.message);
+        } finally {
+        setLoading(false);
+        }
+    }
+
+    if (error) {
+        console.log("Error:" && { error });
+    }
+    
 
     return ( 
         <div className="dashboard">
-
             {/*basic information*/}
             <div className="section-header">
-                <div className="section-title">{uuid}</div>
+                <div className="section-title">{projects.name}</div>
             </div>
             <div className="project-container">
             
@@ -26,7 +94,24 @@ const ProjectDashboard = () => {
                 <div className="section-subtitle">Latest edits on your project</div>
             </div>
             <div className="project-container">
-
+                {actions.slice(0, 3).map((action) => (
+                    <div className="preview-item" >
+                        <div className="preview-content" key={action.id}>
+                            <div className="preview-details">
+                            <div className="preview-title">
+                                <span className="project-name">{action.name}</span>
+                                <span className="recent-text">{action.context}</span>
+                            </div>
+                            </div>
+                            <div className="preview-timestamp">
+                            <TimeStampFormatter
+                                timestamp={action.created_at}
+                                format="date"
+                            />
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
 
             {/*team members*/}
